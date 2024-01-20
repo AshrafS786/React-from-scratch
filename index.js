a1efe25d3eacfcc195ac0aa8c2de31c8190c0a5d
Ashraf Ali <ashrafali786522002@gmail.com.com>
2024-01-20 12:40:09 +0530

    episode-01

---
+++ index.js
@@ -0,0 +1,14 @@
+/* JavaScript Code
+// const heading = document.createElement("h1")
+// heading.innerHTML = "Hello world! This is a Js"
+
+// const root = document.getElementById("root")
+// root.appendChild(heading)
+*/
+
+/*React Coode 
+//     const heading = React.createElement("h1", {}, "Hello world! This is React.");
+
+//     ReactDOM.render(heading, document.getElementById("root"));
+*/
+
